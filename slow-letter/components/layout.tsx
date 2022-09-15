030c0c55acc449926fd15bd13ab06d4f775e61d3
pione3r <jhyong0626@gmail.com>
2022-09-15 14:46:36 +0900

    [feat] 네비게이션바 프로필 버튼 등 UI 구현

--- slow-letter/components/layout.tsx
+++ slow-letter/components/layout.tsx
@@ -1,5 +1,5 @@
 import { Props } from "../types/children-type";
-import NavBar from "./navbar";
+import NavBar from "./navbar/navbar";
 
 export default function Layout({ children }: Props) {
   return (
